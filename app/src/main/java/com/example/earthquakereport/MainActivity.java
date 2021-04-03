3b8ad385ed2adb077fac9ba035b90360ba142fe3
DREAM_DEVELOPER <46447285+developerstrange@users.noreply.github.com>
2021-04-03 09:42:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/earthquakereport/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.earthquakereport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ListView listView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView  = findViewById(R.id.list);
+
+        ArrayList<String> earthquakes = new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+
+        ArrayAdapter<String>  adapter  = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,earthquakes);
+
+        listView.setAdapter(adapter);
+
+    }
+}
